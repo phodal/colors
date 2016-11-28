abac7177e275338f23b58cdd8337b1212e0c1981
Fengda HUANG <h@phodal.com>
2016-11-28 21:08:12 +0800

    Add draw text

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
+
 # color
 
 > A Collection of Phodal's usage in design
 
- Name          |    Value                                                 | BG Example
- --------------|----------------------------------------------------------|--------------
- Metro Blue    | <a><img src="https://phodal.github.io/colors/colors/metro-blue-text.svg"/></a>          | <a><img src="https://phodal.github.io/colors/colors/metro-blue-bg.svg"/></a>
+Name          |    Value                                                 | BG Example
+--------------|----------------------------------------------------------|--------------
+Metro Blue    | <a><img src="https://phodal.github.io/colors/colors/metro-blue-text.svg"/></a>          | <a><img src="https://phodal.github.io/colors/colors/metro-blue-bg.svg"/></a>
 
 
 
--- generator.py
+++ generator.py
@@ -1,6 +1,23 @@
 import svgwrite
+import sys
 
-dwg = svgwrite.Drawing('test.svg', profile='tiny')
-dwg.add(dwg.line((0, 0), (10, 0), stroke=svgwrite.rgb(10, 10, 16, '%')))
-dwg.add(dwg.text('Test', insert=(0, 0.2), fill='red'))
-dwg.save()
+
+def draw_text(file_name, color_name, color):
+    dwg = svgwrite.Drawing('colors/' + file_name + '-bg.svg', height=180, width=30)
+    dwg.add(dwg.text(color_name + ':' + color, insert=(10, 20), fill=color))
+    dwg.save()
+
+
+if __name__ == '__main__':
+    file_name = sys.argv[1]
+    color_name = sys.argv[2]
+    color = sys.argv[3]
+
+    draw_text(file_name=file_name, color_name=color_name, color=color)
+
+    print('---------------------Markdown---------------------')
+    print(color_name + '   '
+          ' | '
+          '<a><img src="https://phodal.github.io/colors/colors/' + file_name + '-text.svg"/></a>          '
+          ' | '
+          '<a><img src="https://phodal.github.io/colors/colors/' + file_name + '-bg.svg"/></a>')
